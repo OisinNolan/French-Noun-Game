8826f368fdd216a931bcf806d5a69de9797f6265
Maddie <maddie.comtois@gmail.com>
2019-02-12 20:59:41 +0000

    added words to list
    Still having problems adding accents

--- dictionaryWords.js
+++ dictionaryWords.js
@@ -1,39 +1,73 @@
 var dict = [];
-dict.push({
-    word:   "livre",
-    gender: 'm',
-    translation: "book"
-},
-{
-    word:   "chat",
-    gender: 'm',
-    translation: "cat"
-},
-{
-    word:   "voiture",
-    gender: 'f',
-    translation: "car"
-},
-
-{
-    word:   "poisson",
-    gender: 'm',
-    translation: "fish"
-},
-{
-    word:   "chapeau",
-    gender: 'm',
-    translation: "hat"
-},
-{
-    word:   "ordinateur",
-    gender: 'f',
-    translation: "computer"
-},
-{
-    word:   "maison",
-    gender: 'f',
-    translation: "house"
-}
-
-);
+dict.push(
+{word:  "livre",      gender: 'm',    translation: "book"},
+{word:  "chat",       gender: 'm',    translation: "cat"},
+{word:  "voiture",    gender: 'f',    translation: "car"},
+{word:  "poisson",    gender: 'm',    translation: "fish"},
+{word:  "chapeau",    gender: 'm',    translation: "hat"},
+{word:  "ordinateur", gender: 'f',    translation: "computer"},
+{word:  "maison",     gender: 'f',    translation: "house"},
+{word:  "cage",     gender: 'f',    translation: "cage"},
+{word:  "nage",     gender: 'f',    translation: "swimming"},
+{word:  "page",     gender: 'f',    translation: "page"},
+{word:  "rage",     gender: 'f',    translation: "rage, rabies"},
+{word:  "fac",     gender: 'f',    translation: "university"},
+{word:  "fourmi",     gender: 'f',    translation: "ant"},
+{word:  "foi",     gender: 'f',    translation: "faith"},
+{word:  "loi",     gender: 'f',    translation: "law"},
+{word:  "fin",     gender: 'f',    translation: "ending"},
+{word:  "brebis",     gender: 'f',    translation: "ewe, sheep"},
+{word:  "souris",     gender: 'f',    translation: "mouse"},
+{word:  "oasis",     gender: 'f',    translation: "oasis"},
+{word:  "liste",     gender: 'f',    translation: "list"},
+{word:  "faim",     gender: 'f',    translation: "hunger"},
+{word:  "m&eacutet&eacuteo",     gender: 'f',    translation: "weather"},
+{word:  "moto",     gender: 'f',    translation: "motorcycle"},
+{word:  "mer",     gender: 'f',    translation: "sea"},
+{word:  "tour",     gender: 'f',    translation: "tower"},
+{word:  "for&ecirct",     gender: 'f',    translation: "forest"},
+{word:  "nuit",     gender: 'f',    translation: "night"},
+{word:  "part",     gender: 'f',    translation: "share, portion"},
+{word:  "fen&ecirctre",     gender: 'f',    translation: "window"},
+{word:  "vitre",     gender: 'f',    translation: "window pane"},
+{word:  "rencontre",     gender: 'f',    translation: "meeting, match"},
+{word:  "montre",     gender: 'f',    translation: "watch, show"},
+{word:  "eau",     gender: 'f',    translation: "water"},
+{word:  "vertu",     gender: 'f',    translation: "vertue"},
+{word:  "stade",     gender: 'm',    translation: "stadium, stage"},
+{word:  "espace",     gender: 'm',    translation: "space, area"},
+{word:  "exercice",     gender: 'm',    translation: "exercise"},
+{word:  "b&eacuten&eacutefice",     gender: 'm',    translation: "profit, benefit"},
+{word:  "office",     gender: 'm',    translation: "office"},
+{word:  "dentifrice",     gender: 'm',    translation: "toothpaste"},
+{word:  "service",     gender: 'm',    translation: "service"},
+{word:  "pouce",     gender: 'm',    translation: "thumb"},
+{word:  "lyc&eacutee",     gender: 'm',    translation: "secondary school"},
+{word:  "mus&eacutee",     gender: 'm',    translation: "museum"},
+{word:  "bonheur",     gender: 'm',    translation: "happiness"},
+{word:  "incendie",     gender: 'm',    translation: "fire, blaze"},
+{word:  "g&eacutenie",     gender: 'm',    translation: "genie, genius"},
+{word:  "foie",     gender: 'm',    translation: "liver"},
+{word:  "parapluie",     gender: 'm',    translation: "umbrella"},
+{word:  "domaine",     gender: 'm',    translation: "field, estate, domain"},
+{word:  "capitaine",     gender: 'm',    translation: "captain"},
+{word:  "patrimoine",     gender: 'm',    translation: "heritage, estate, inheritance"},
+{word:  "magazine",     gender: 'm',    translation: "magazine"},
+{word:  "camion",     gender: 'm',    translation: "truck"},
+{word:  "espion",     gender: 'm',    translation: "spy"},
+{word:  "avion",     gender: 'm',    translation: "airplane"},
+{word:  "pl&eacutebiscite",     gender: 'm',    translation: "referendum"},
+{word:  "g&icircte",     gender: 'm',    translation: "vacation rental, lair"},
+{word:  "ermite",     gender: 'm',    translation: "hermit, recluse"},
+{word:  "rite",     gender: 'm',    translation: "right, ritual"},
+{word:  "site",     gender: 'm',    translation: "site, place"},
+{word:  "vase",     gender: 'm',    translation: "vase"},
+{word:  "mailaise",     gender: 'm',    translation: "dizzy spell, unease"},
+{word:  "suspense",     gender: 'm',    translation: "suspense"},
+{word:  "inverse",     gender: 'm',    translation: "opposite, reverse"},
+{word:  "carrosse",     gender: 'm',    translation: "carriage"},
+{word:  "pamplemousse",     gender: 'm',    translation: "grapefruit"},
+{word:  "squelette",     gender: 'm',    translation: "skeleton"},
+{word:  "coude",     gender: 'm',    translation: "elbow"},
+{word:  "dinosaure",     gender: 'm',    translation: "dinosaur"},
+{word:  "murmure",     gender: 'm',    translation: "whisper"}, );
