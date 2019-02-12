b30fc498755cc324864d760c3ffe4772eb5821ce
Maddie <maddie.comtois@gmail.com>
2019-02-12 20:24:20 +0000

    put words in a seperate js file

---
+++ dictionaryWords.js
@@ -0,0 +1,39 @@
+var dict = [];
+dict.push({
+    word:   "livre",
+    gender: 'm',
+    translation: "book"
+},
+{
+    word:   "chat",
+    gender: 'm',
+    translation: "cat"
+},
+{
+    word:   "voiture",
+    gender: 'f',
+    translation: "car"
+},
+
+{
+    word:   "poisson",
+    gender: 'm',
+    translation: "fish"
+},
+{
+    word:   "chapeau",
+    gender: 'm',
+    translation: "hat"
+},
+{
+    word:   "ordinateur",
+    gender: 'f',
+    translation: "computer"
+},
+{
+    word:   "maison",
+    gender: 'f',
+    translation: "house"
+}
+
+);
